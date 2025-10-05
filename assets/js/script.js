1a850119445aca47714a85427b1c77d46b8eea48
kePREPRE <cuentaprincipalkeylor@gmail.com>
2025-10-05 09:44:34 -0600

    Primer commit - sitio SkyAdventure

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Bootstrap validation
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+  forms.forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
